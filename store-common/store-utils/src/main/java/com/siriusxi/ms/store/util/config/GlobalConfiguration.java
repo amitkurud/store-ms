101864de36083e98312a427cd0b52bf39cc127f9
amitkurud <amit.kurud@gmail.com>
2020-04-28 18:34:03 +0200

    Date time fix to Event.java to pass all tests.

--- store-common/store-utils/src/main/java/com/siriusxi/ms/store/util/config/GlobalConfiguration.java
+++ store-common/store-utils/src/main/java/com/siriusxi/ms/store/util/config/GlobalConfiguration.java
@@ -25,8 +25,8 @@ public class GlobalConfiguration ***REMOVED***
   public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() ***REMOVED***
     return builder ->
         builder
-            .visibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
-            // human readable
-            .indentOutput(true);
+            .visibility(
+                    PropertyAccessor.FIELD,
+                    JsonAutoDetect.Visibility.ANY);
 ***REMOVED***
 ***REMOVED***
